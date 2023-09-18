9f2aba95f4f383d943af5ea959cf514e6e96c6dd
tanmaysihare <tanmaysihare@gmail.com>
2023-09-18 06:41:40 +0530

    setting auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,31 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    token:'',
-    isLoggedIn: false,
-    login: (token)=> {},
-    logout: ()=> {},
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
 });
+
 export const AuthContextProvider = (props) => {
-    const initialToken = localStorage.getItem('token');
-    const [token, setToken] = useState(initialToken);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
+  const [logoutTimer, setLogoutTimer] = useState(null); // Store the logout timer ID
 
-    const userIsLoggedIn = !!token;
-    const loginHandler = (token)=> {
-        setToken(token);
-        localStorage.setItem('token',token);
-    };
-    const logoutHandler = ()=> {
-        setToken(null);
-        localStorage.removeItem('token');
-    };
-    const contextValue = {
-        token: token,
-        isLoggedIn: userIsLoggedIn,
-        login: loginHandler,
-        logout: logoutHandler,
+  const userIsLoggedIn = !!token;
 
+  // Reset the logout timer whenever there's user activity
+  const resetLogoutTimer = () => {
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
     }
-    return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>;
+    const newLogoutTimer = setTimeout(() => {
+      logoutHandler();
+    }, 5 * 60 * 1000); // 5 minutes in milliseconds
+    setLogoutTimer(newLogoutTimer);
+  };
+
+  useEffect(() => {
+    // Set the initial logout timer when the component mounts
+    resetLogoutTimer();
+
+    // Cleanup the timer when the component unmounts
+    return () => {
+      if (logoutTimer) {
+        clearTimeout(logoutTimer);
+      }
+    };
+  }, [token]); // Run this effect whenever the token changes
+
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+    resetLogoutTimer(); // Reset the timer after login
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
+
 export default AuthContext;
