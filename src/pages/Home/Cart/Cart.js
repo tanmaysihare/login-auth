74c375b2bef2c4f62fb8f2f536622e0ecd686d6e
tanmaysihare <tanmaysihare@gmail.com>
2023-09-20 09:11:30 +0530

    making cart user spesific

--- src/pages/Home/Cart/Cart.js
+++ src/pages/Home/Cart/Cart.js
@@ -1,45 +1,84 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios'; // Import axios
 
-import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../store/cart-context';
+import AuthContext from '../../../store/auth-context';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const authCtx = useContext(AuthContext);
+  const [cartItems, setCartItems] = useState([]);
+  const hasItems = cartItems.length > 0;
 
-  const totalAmount = `Rs.${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length > 0;
+  useEffect(() => {
+    // Define your user's email (replace 'user@example.com' with the actual email)
+    const userEmail = authCtx.token;
+
+    // Make a GET request to retrieve cart items when the component mounts
+    axios
+      .get(`https://crudcrud.com/api/cart/${userEmail}`)
+      .then((response) => {
+        setCartItems(response.data); // Update cart items in the component state
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('Error fetching cart items:', error);
+      });
+  }, [authCtx.token]);
 
   const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id);
+    // Define your user's email (replace 'user@example.com' with the actual email)
+    const userEmail = authCtx.token;
+
+    // Make a DELETE request to remove a cart item
+    axios
+      .delete(`https://crudcrud.com/api/cart/${userEmail}/${id}`)
+      .then(() => {
+        // Remove the item from the component state
+        setCartItems((prevCartItems) => prevCartItems.filter((item) => item.id !== id));
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('Error removing cart item:', error);
+      });
   };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({ ...item, amount: 1 });
+    // Define your user's email (replace 'user@example.com' with the actual email)
+    const userEmail = authCtx.token;
+
+    // Make a POST request to add a cart item
+    axios
+      .post(`https://crudcrud.com/api/cart/${userEmail}`, item)
+      .then(() => {
+        // Add the item to the component state
+        setCartItems((prevCartItems) => [...prevCartItems, item]);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('Error adding cart item:', error);
+      });
   };
 
-  const cartItems = (
-    <ul className={classes['cart-items']}>
-      {cartCtx.items.map((item) => (
-        <CartItem
-          key={item.id}
-          name={item.name}
-          amount={item.amount}
-          price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item)}
-        />
-      ))}
-    </ul>
-  );
+  const cartItemsList = cartItems.map((item) => (
+    <CartItem
+      key={item.id}
+      name={item.name}
+      amount={item.amount}
+      price={item.price}
+      onRemove={() => cartItemRemoveHandler(item.id)}
+      onAdd={() => cartItemAddHandler({ ...item, amount: 1 })}
+    />
+  ));
 
   return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
+    <div className={classes.cart}>
+      <ul className={classes['cart-items']}>{cartItemsList}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{totalAmount}</span>
+        <span>{`Rs.${cartCtx.totalAmount.toFixed(2)}`}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
@@ -47,7 +86,7 @@ const Cart = (props) => {
         </button>
         {hasItems && <button className={classes.button}>Order</button>}
       </div>
-    </Modal>
+    </div>
   );
 };
 
